c6f9b30dc869c3257db3018fdb05f0b495ec46d6
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2016-12-23 17:08:59 +0800

    better

--- index.js
+++ index.js
@@ -14,36 +14,41 @@ request(msaapi.getRequestOptions(api.search.top, {}))
     data = data.toString();
     let {search: {searching}} = JSON.parse(data);
     console.log(searching);
+
     let unCollectedIds = searching.map(s => s.point_id);
     unCollectedIds = [1, 2, 3, 4, 5, 6].filter(s => !~unCollectedIds.indexOf(s));
 
+    // Restarting treasures.
     unCollectedIds.map(id => {
+      console.log(`restarting.... ${id}`);
       request(msaapi.getRequestOptions(api.search.start, { 'search_point': id }))
         .pipe(zlib.createGunzip())
-        .pipe(process.stdout);
+        .pipe(bl(function (err, data) {
+          data = JSON.parse(data.toString());
+          if (data.response.error_code != 0) {
+            console.log(`ERROR when restarting ${id}`);
+          } else {
+            console.log(`restarting.... ${id} done.`);
+          }
+        }));
     });
 
+
+
+    // Collecting treasures.
     searching.map(s => {
       if (new Date(s.event) > Date.now() + 3600 * 1000) return;
-      console.log('collecting.... restarting...');
-      getTreasure(s.id);
+
+      console.log(`collecting.... ${s.point_id}`);
+      request(msaapi.getRequestOptions(api.search.get, { 'search_point': s.point_id }))
+        .pipe(zlib.createGunzip())
+        .pipe(bl(function (err, data) {
+          data = JSON.parse(data.toString());
+          if (data.response.error_code != 0) {
+            console.log(`ERROR when collecting ${s.point_id}`);
+          } else {
+            console.log(`collecting.... ${s.point_id} done.`);  
+          }
+        }));
     });
   }));
-
-
-function getTreasure(point_id) {
-  return request(msaapi.getRequestOptions(api.search.get, { 'search_point': point_id }))
-    .then(request(msaapi.getRequestOptions(api.search.start, { 'search_point': s.point_id })));
-}
-
-// ['1', '2', '3', '4', '5', '6'].map(pt => {
-//   request(msaapi.getRequestOptions(api.search.get, { 'search_point': pt }))
-//     .pipe(zlib.createGunzip())
-//     .pipe(bl(function (err, data) {
-//       data = data.toString();
-//       data = JSON.parse(data);
-//       if (data.response.error_code === 0) {
-//         request(msaapi.getRequestOptions(api.search.start, { 'search_point': pt })) 
-//       }
-//     }));
-// })
