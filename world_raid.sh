2944b086228840c4ab2d080ee53616236e9bab90
ssarcandy <ssarcandy@gmail.com>
2017-02-01 22:54:28 +0800

    add world raid script

---
+++ world_raid.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ./utils/send_request.sh
+
+echo "Raid STANDARD 1-1 and ELITE 1-1"
+
+send_request \
+    "num=1&stage_id=1" \
+    "https://msaapi.snkplaymore.info/world_map/raid/?kpi1=1&kpi2=1" > /dev/null
+
+send_request \
+    "num=1&stage_id=71" \
+    "https://msaapi.snkplaymore.info/world_map/raid/?kpi1=71&kpi2=1" > /dev/null
+
